7480f50ac6cc03d10bae4d663ac5a951390b4834
Aaron Morris <aaron.morris@logansferry.com>
2013-10-15 06:18:59 -0400

    Added blocking of screen during long AJAX operations.

--- TradingSuite/TradingSuite.WebApp/Areas/StockData/Scripts/ManageStocks.js
+++ TradingSuite/TradingSuite.WebApp/Areas/StockData/Scripts/ManageStocks.js
@@ -4,7 +4,6 @@
     // Invoke the Update Is Excluded controller action via AJAX.
     $("input[name='Value.IsExcluded']").change(function () {
         var selectionJson = { ticker: $(this).val(), isExcluded: $(this).is(":checked") };
-
         $.ajax({
             type: "POST",
             traditional: true,
@@ -22,12 +21,16 @@
     // UpdateButton_Click
     // Invoke the Update Stocks controller action via AJAX.
     $("#updateButton").click(function () {
+
+        $.blockUI({ message: '<h2><img style="vertical-align:middle;" src="../../Images/busy.gif" /> Updating stock list from web...</h1>' });
+        
         $.ajax({
             type: "POST",
             traditional: true,
             url: '/StockData/ManageStocks/UpdateStocks',
             dataType: "json",
             success: function (data) {
+                $.unblockUI();
                 alert(data + " stocks were added.");
                 window.location = '/StockData/ManageStocks';
             }
